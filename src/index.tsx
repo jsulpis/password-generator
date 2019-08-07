139c6b484d1a9ddba24e31c7f4747cbdab71be9b
Julien SULPIS <julien.sulpis@gmail.com>
2019-08-07 20:28:47 +0200

    feat: add Argon design system by Creative Tim

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./assets/vendor/nucleo/css/nucleo.css";
+import "./assets/vendor/font-awesome/css/font-awesome.min.css";
+import "./assets/scss/argon-design-system-react.scss";
+import './index.scss';
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
